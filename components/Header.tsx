868b956d1e3c77cb1ad31fed14d9e58c22d405c8
YeloowCan <942789838@qq.com>
2023-04-22 21:47:56 +0800

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function Header() {
   return (
-    <header className="flex h-12 bg-sky-600 overflow-hidden">
-      <Image src="/logo.jpg" alt="罐头博客" width={60} height={30} />
-      <span className="ml-2">罐头音乐</span>
+    <header className="flex h-12 bg-sky-600 overflow-hidden items-center">
+      <Head>
+        <title>罐头博客</title>
+      </Head>
+      <Image
+        src="/logo.png"
+        alt="罐头博客"
+        width={50}
+        height={30}
+        className="ml-4"
+      />
+      <span className="ml-2">罐头博客</span>
     </header>
   );
 }
