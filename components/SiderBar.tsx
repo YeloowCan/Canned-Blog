d928d6f8861132ae5447cc19b5dda4ab9313a84c
YeloowCan <942789838@qq.com>
2023-05-05 21:54:09 +0800

    avatar

--- components/SiderBar.tsx
+++ components/SiderBar.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
 import Card from './Card';
 
 export default function SideBar() {
   return (
-    <Card height={200} classname="w-64">
-      SiderBar
+    <Card height={200} classname="w-64 ">
+      <div className="flex justify-center mt-9">
+        <Image
+          src="/avatar.jpg"
+          width={100}
+          height={100}
+          alt="头像"
+          className="rounded-full"
+        />
+      </div>
     </Card>
   );
 }
